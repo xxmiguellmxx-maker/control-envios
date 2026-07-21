24958055417e8d3b4ec52fa6ff74c4151dbab78f
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-07-21 16:16:20 -0600

    Fix syntax error in config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+/************************************************
+ * CONFIGURACIÓN GENERAL
+ ************************************************/
+
 const CONFIG = {
 
     API_URL: "https://script.google.com/macros/s/AKfycbwCqfI-hK4DkV5APR2gCVML-s1vEZfGrYxakPYSsT8Bx4cwYIVo1OoSQOfjOz2HKsQ/exec",
