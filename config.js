cee3ef463e20cf38f2192aeadd063503bb64c748
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-07-21 15:44:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
 
-    API_URL httpsscript.google.commacrossAKfycbwCqfI-hK4DkV5APR2gCVML-s1vEZfGrYxakPYSsT8Bx4cwYIVo1OoSQOfjOz2HKsQexec,
+    API_URL: "https://script.google.com/macros/s/AKfycbwCqfI-hK4DkV5APR2gCVML-s1vEZfGrYxakPYSsT8Bx4cwYIVo1OoSQOfjOz2HKsQ/exec",
 
-    APP_NAME Control de Envíos,
+    APP_NAME: "Control de Envíos",
 
-    VERSION 1.0
+    VERSION: "1.0"
 
 };
