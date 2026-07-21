f313a91f7cac96ff02f995892a1554802d29dc64
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-07-21 14:52:25 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+
+    API_URL httpsscript.google.commacrossAKfycbwCqfI-hK4DkV5APR2gCVML-s1vEZfGrYxakPYSsT8Bx4cwYIVo1OoSQOfjOz2HKsQexec,
+
+    APP_NAME Control de Envíos,
+
+    VERSION 1.0
+
+};
