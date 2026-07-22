f8bf0073726ae62a508baaeb1aab5545a820f310
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-07-22 10:59:27 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,17 @@
 /************************************************
  * CONFIGURACIÓN GENERAL
+ * CONTROL DE ENVÍOS
  ************************************************/
 
 const CONFIG = {
 
+    // URL de la aplicación web de Google Apps Script
     API_URL: "https://script.google.com/macros/s/AKfycbwu8sYOzxWJZIjKGY1itJ1evrxZ34XsWD2UacgIbVtUOXeht_RzSvXxUbGqOFBgeQqA/exec",
 
+    // Nombre de la aplicación
     APP_NAME: "Control de Envíos",
 
-    VERSION: "1.0"
+    // Versión
+    VERSION: "2.0"
 
 };
