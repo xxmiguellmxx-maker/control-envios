b0713a40d796fc4aafb72330acf8ed0d40cb7314
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-07-22 12:52:24 -0600

    Update API_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
 
     // URL de la aplicación web de Google Apps Script
-    API_URL: "https://script.google.com/macros/s/AKfycbwu8sYOzxWJZIjKGY1itJ1evrxZ34XsWD2UacgIbVtUOXeht_RzSvXxUbGqOFBgeQqA/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbwyuhkcgJ9v41g8h5esM-Ikg8s0_a0b8xFugk-eXcl0vH9dYZFiYVbZgTmSK01pHs3v/exec",
 
     // Nombre de la aplicación
     APP_NAME: "Control de Envíos",
