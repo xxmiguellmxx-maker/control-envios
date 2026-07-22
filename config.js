869b57e4b3c45092a38cb4770b8c4a73a80d707d
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-07-22 16:30:10 -0600

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
 
     // URL de la aplicación web de Google Apps Script
-    API_URL: "https://script.google.com/macros/s/AKfycbwyuhkcgJ9v41g8h5esM-Ikg8s0_a0b8xFugk-eXcl0vH9dYZFiYVbZgTmSK01pHs3v/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbxQcWheZjO41FrOjm_RsRUFP3UMAIME7Ea0C2jLgBgCxkNuRnJqYYBAqtf97Zy3XgpL/exec",
 
     // Nombre de la aplicación
     APP_NAME: "Control de Envíos",
