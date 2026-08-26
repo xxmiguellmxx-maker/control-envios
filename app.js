fef4188b75d36f284b1ef1f80063f28ffc79724a
xxmiguellmxx-maker <xxmiguellmxx@gmail.com>
2026-08-26 16:43:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ async function iniciarScanner() {
             {
                 fps: 10,
               qrbox: {
-    width: 260,
-    height: 90
+    width: 270,
+    height: 100
 }
             },
             codigoDetectado,
